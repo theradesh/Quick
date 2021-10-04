480534e3391cb2087292ba020b59a81dff8a943b
Theradesh Chaiyapromma <theradesh.cha@2c2pexternal.com>
2021-10-04 17:38:28 +0700

    Finish

---
+++ Quick/Quick/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Quick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string: ");
+
+            string input = Console.ReadLine();
+            Console.WriteLine($"Given input is \"{input}\"");
+
+            string output = string.Empty;
+            foreach (char char_ in input)
+            {
+                if (char_ >= '0' && char_ <= '9')
+                {
+                    output += char_;
+                }
+            }
+
+            bool ouputNoInt = true;
+            foreach (char char_ in output)
+            {
+                ouputNoInt = false;
+                break;
+            }
+
+            if (ouputNoInt)
+                Console.WriteLine($"The output doesn't have any integer.");
+            else
+                Console.WriteLine($"The output should be {output} in integer.");
+        }
+    }
+}
